4682ab720e60c6ae677ff480750b5bd17ad1719e
Raquel Santos <raquelmrsantos@gmail.com>
2026-02-12 00:46:32 +0000

    feat: add file upload component to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
+"use client";
+
+import { Dataset } from "@/types";
+import { FileUpload } from "./components/FileUpload";
+import { useState } from "react";
+
 export default function Home() {
+  const [dataset, setDataset] = useState<Dataset | null>(null);
+
+  const handleFileProcessed = (processedDataset: Dataset) => {
+    setDataset(processedDataset);
+    console.log('Dataset processed:', processedDataset);
+  };
+
+
   return (
     <div className="">
       <main className="">
+        <FileUpload onFileProcessed={handleFileProcessed} />
        
       </main>
     </div>
